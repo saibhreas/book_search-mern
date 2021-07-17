e2140ce07564b21ceecb3e037c2b9b69a5f4e3a9
siobhan <sr@saibhreas.com>
2021-07-17 10:44:00 -0400

    all of the drop/adds of nodes and file rebuild

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,15 @@
+//grabbed from: C:\Users\kern\Desktop\RUT-VIRT-FSF-FT-04-2021-U-LOL\21-MERN\01-Activities\25-Ins_Resolver-Context\server\utils\auth.js
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'mysecretssshhhhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;//add console.logs for all 3 of these
 
+    // We split the token string into an array and return actual token
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -17,18 +18,20 @@ module.exports = {
       return req;
     }
 
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      console.log(data);//re.user.email, name, id
       req.user = data;
     } catch {
       console.log('Invalid token');
     }
 
+    // return the request object so it can be passed to the resolver as `context`
     return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
