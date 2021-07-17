cc433b5d6986249f5016c8149b8955d9267fd469
siobhan <sr@saibhreas.com>
2021-07-17 09:23:30 -0400

    build unstabel. starting over again

--- server/Oldserver.js
+++ server/Oldserver.js
@@ -4,7 +4,24 @@ const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+//const routes = require('./routes');
+//Removed becaseu now using Apollo Graphql
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 async function startApolloServer() {
 
   const server = new ApolloServer({
@@ -31,19 +48,11 @@ async function startApolloServer() {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
-  // app.use(routes);
-
   await new Promise(resolve => db.once('open', () => resolve()));
   await new Promise(resolve => app.listen({ port: PORT }, resolve));
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   return { server, app };
 }
 
-startApolloServer()
-  .then(({ server, app }) => {
-    console.log('Done starting.');
-  })
-  .catch(err => {
-    console.log('Error while starting Apollo server: ', err);
-  })
+startApolloServer();
 
