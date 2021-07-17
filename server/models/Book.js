5d87c3596736f6b34b3dc2664a0e366725814962
siobhan <sr@saibhreas.com>
2021-07-17 14:31:23 -0400

    Ask BCS bittersweet.  solution negated the move to graphql.  So happy to try, we ended before result.  Then I realized why I had the const User = model('User', userSchema); lin in user.   back to same space. cannot sign up. graphql does not like something in y schema/models

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const Book = model('Book', bookSchema);
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
@@ -29,4 +28,5 @@ const bookSchema = new Schema({
   },
 });
 
+const Book = model('Book', bookSchema);
 module.exports = Book;
