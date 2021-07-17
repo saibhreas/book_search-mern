8faae173ca2efd3dd3025c5e08cf922dbda94195
siobhan <sr@saibhreas.com>
2021-07-17 09:51:42 -0400

    beginning build with apollo-server-express graphql from start insted of going between worlds to get it to work

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+async function startApolloServer() {
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  server.applyMiddleware({ app });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  // app.use(routes);
+
+  await new Promise(resolve => db.once('open', () => resolve()));
+  await new Promise(resolve => app.listen({ port: PORT }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  return { server, app };
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer()
+  .then(({ server, app }) => {
+    console.log('Done starting.');
+  })
+  .catch(err => {
+    console.log('Error while starting Apollo server: ', err);
+  })
