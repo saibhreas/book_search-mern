6820f1c081081b1334f08d05e83e9f6b291acd42
siobhan <sr@saibhreas.com>
2021-07-17 10:49:52 -0400

    lifted Schemas: including: index, typedefs, and resolvers.  Refcactoring for book search.  Using becaseu I know they are pathed correctly for the server

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
+
+//lifeted from C:\Users\kern\Desktop\RUT-VIRT-FSF-FT-04-2021-U-LOL\21-MERN\01-Activities\25-Ins_Resolver-Context\server\schemas\index.js
