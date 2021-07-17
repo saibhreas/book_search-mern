968b919765711f6c9a68c5c9d862151ab9a6ac9b
siobhan <sr@saibhreas.com>
2021-07-17 09:09:14 -0400

    wokring in server end to itegrate graphql

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+//Login_user
+export const LOGIN_USER =  gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+//ADD user
+export const ADD_USER =  gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+
+//Save Search
+export const SAVE_BOOK =  gql`
+    mutation saveBook($input: savedBook!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+//Delete search
+export const REMOVE_BOOK =  gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
